13262f352a52beacd0fa4aab98fecf362fa306d5
Gavrushoves <gavruhovesgavr@gmail.com>
2018-12-05 11:24:54 +0300

    stuвy project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	$('.slider').slick({
+		autoplay: true,
+		autoplaySpeed: 1500,
+		pauseOnHover: false
+	});
+	$('.services-slider').slick({
+		autoplay: true,
+		autoplaySpeed: 1500,
+		pauseOnHover: false
+	});
+});
+
